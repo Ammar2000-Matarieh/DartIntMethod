c1829e2a06c10b2aeea86d86727e0a28a5d35161
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2025-04-21 02:03:13 +0300

    Update

--- main.dart
+++ main.dart
@@ -3,6 +3,8 @@ void main() {
 
   int myage = 32;
 
+  int myyear = 1993;
+
   double myhighest = -171.5;
 
   print(myName[0].toUpperCase());
@@ -12,6 +14,10 @@ void main() {
   print(myhighest.round());
   print(myhighest.isNegative);
   print(myhighest.abs());
+  // update changes in code :
+  int myageAfter10YEar = myage + 10;
+
+  print("my age after 10 years is $myageAfter10YEar");
 
   // ceil => +1 :
   // floor => remove any comma :
