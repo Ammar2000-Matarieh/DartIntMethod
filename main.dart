9d5b59e8438c8c4b8f0f662645d58baf80107454
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2025-04-21 01:58:12 +0300

    update

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main() {
+  String myName = "abedalraheem";
+
+  int myage = 32;
+
+  double myhighest = -171.5;
+
+  print(myName[0].toUpperCase());
+
+  print(myhighest.ceil());
+  print(myhighest.floor());
+  print(myhighest.round());
+  print(myhighest.isNegative);
+  print(myhighest.abs());
+
+  // ceil => +1 :
+  // floor => remove any comma :
+  // round => 0.5 => بقرب للاقرب رقم صحيح  :
+  // =============> 0.4 اقرب للرقم الاسفل :
+
+  // isNegative => number - :
+
+  // abc => قيمة مطلقه :
+}
